b70080725523f091f4641fcdc413983d66dd2a92
Nahim <nahim_hsn@outlook.com>
2025-07-28 17:46:13 +0100

    Converted extract code into OOP and added logging

--- bluesky_etl/extract.py
+++ bluesky_etl/extract.py
@@ -1,12 +1,22 @@
 """Extract script to read live data from the Bluesky firehose API"""
 from atproto import FirehoseSubscribeReposClient, parse_subscribe_repos_message, CAR, models
 import argparse
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
-def firehose_stream(topic: str):
-    """Wrapper function that creates a message handler to filters posts by a given topic."""
+class BlueSkyFirehose:
+    """Tracks Bluesky messages that contain a given topic keyword"""
 
-    def firehose(message):
+    def __init__(self, topic: str):
+        self.topic = topic.lower()
+        self.client = FirehoseSubscribeReposClient()
+
+    def extract_message(self, message):
         """Reads a message from the stream and prints the raw output if it is a post"""
 
         commit = parse_subscribe_repos_message(message)
@@ -23,15 +33,18 @@ def firehose_stream(topic: str):
                     continue
                 if raw.get("$type") == "app.bsky.feed.post":
                     post_text = raw.get("text").lower()
-                    if topic in post_text:
-                        # Do transform  and load on each raw message here. Similar to how ETL was done in Museum Kafka
-                        print(raw)
-    return firehose
+                    if self.topic in post_text:
+                        # Do transform  and load on each raw message extracted here. Similar to how ETL was done in Museum Kafka.
+                        logging.info(raw)
+
+    def start(self):
+        """Starts the firehose stream"""
+        self.client.start(self.extract_message)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("topic", help="Submit a topic to track")
     args = parser.parse_args()
-    client = FirehoseSubscribeReposClient()
-    client.start(firehose_stream(args.topic))
+    firehose = BlueSkyFirehose(args.topic)
+    firehose.start()
