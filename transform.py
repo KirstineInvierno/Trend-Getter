ff26df8e25496974370df2a6ae38e9016d206025
jacoblewis046 <jacoblewis046@gmail.com>
2025-07-29 09:58:49 +0100

    pylint

--- transform.py
+++ transform.py
@@ -1,38 +1,72 @@
+"""
+transform script:
+takes a message extracted from the API and transforms it to a 
+DataFrame ready to load into the database
+"""
 from datetime import datetime
 import pandas as pd
 from transformers import pipeline
 
 
-sentiment = pipeline(
-    model="finiteautomata/bertweet-base-sentiment-analysis")
+def get_sentiment(text: str) -> dict:
+    """
+    Uses an LLM to analyse a text snippet and suggest its sentiment
+    returns a dict with:
+        label - 'POS', 'NEG' or 'NEU'
+        score - certainty score from 0-1 (float)
 
-
-def get_sentiment(text):
+    uses finiteautomata/bertweet-base-sentiment-analysis model which
+    is finetuned for tweets
+    """
+    sentiment = pipeline(
+        model="finiteautomata/bertweet-base-sentiment-analysis")
     return max(sentiment(text), key=lambda x: x.get("score"))
 
 
-def get_text(message):
-    return "BBC Radio 1\nNewsbeat @ 12:45\n\nEngland's Lionesses go back-to-back as they win the Women's Euros for the second time, beating Spain on penalties in Switzerland."
+def get_text(message: dict) -> str:
+    """extracts the text from a message that was posted"""
+    return """
+    BBC Radio 1
+    Newsbeat @ 12:45
+    
+    England's Lionesses go back-to-back as they win the Women's Euros for \
+        the second time, beating Spain on penalties in Switzerland.
+    """
 
 
-def get_timestamp(message):
+def get_timestamp(message: dict) -> datetime:
+    """extracts the timestamp from a message that was posted"""
     return datetime.now()
 
 
-def get_topics():
+def get_topics() -> list[str]:
+    """
+    returns a list of topics subscribed to by users 
+    (will need to access db for this in final version)
+    """
     return ["football", "england", "spain", "cricket", "trump"]
 
 
-def topics_in_text(text, topics):
+def topics_in_text(text: str, topics: list[str]) -> list[str] | None:
+    """
+    deciphers which topics from a given list of topics are 
+    in a given string
+
+    in a future version could add LLM zero-shot classification functionality
+    """
     topics_in = []
     for topic in topics:
         if topic.lower() in text.lower():
             topics_in.append(topic)
 
+    if len(topics_in) < 1:
+        return None
+
     return topics_in
 
 
-def make_df(topic, sentiment, timestamp):
+def make_df(topic: str, sentiment: dict, timestamp: datetime) -> pd.DataFrame:
+    """creates DataFrame with data in the right format for loading"""
 
     df = pd.DataFrame({
         "topic": [topic],
@@ -43,12 +77,18 @@ def make_df(topic, sentiment, timestamp):
     return df
 
 
-def transform(message):
+def transform(message: dict) -> pd.DataFrame:
+    """
+    takes a message extracted from the API and transforms it to a 
+    DataFrame ready to load into the database
+    """
     text = get_text(message)
     timestamp = get_timestamp(message)
 
     topics = get_topics()
     topics_in = topics_in_text(text, topics)
+    if not topics_in:
+        return None
     sentiment = get_sentiment(text)
 
     dfs = []
@@ -60,9 +100,7 @@ def transform(message):
 
 
 def main():
-    # text = "I'll stick with The SNP. Don't need a new centre left party run from England for England competing for votes in Scotland thanks."
-    # print(get_sentiment(text))
-
+    """main function"""
     print(transform(None))
 
 
