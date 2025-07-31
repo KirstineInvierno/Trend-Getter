4a828ec37a5ade7736304dc08e96111f9fc4a646
Nahim <nahim_hsn@outlook.com>
2025-07-31 10:56:46 +0100

    formatted terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,9 +121,9 @@ resource "aws_security_group" "trendgetter_ec2_sg" {
 
 
 resource "aws_instance" "ec2" {
-  ami                         = "ami-0f4f4482537714bd9" 
+  ami                         = "ami-0f4f4482537714bd9"
   instance_type               = "t3.nano"
-  subnet_id                   = data.aws_subnet.public_1.id  
+  subnet_id                   = data.aws_subnet.public_1.id
   associate_public_ip_address = true
   key_name                    = aws_key_pair.trendgetter_ec2_key.key_name
   vpc_security_group_ids      = [aws_security_group.trendgetter_ec2_sg.id]
