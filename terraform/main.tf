86e53bd05361e6bbae1121e67c4c8354d16ea087
Nahim <nahim_hsn@outlook.com>
2025-07-31 10:45:40 +0100

    Created terraform code for creating an EC2 and a key pair

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,3 +81,54 @@ resource "aws_db_instance" "postgresql" {
   backup_retention_period = 7
   deletion_protection     = false
 }
+
+resource "tls_private_key" "trendgetter_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "trendgetter_ec2_key" {
+  key_name   = "c18-trendgetter-kp"
+  public_key = tls_private_key.trendgetter_key.public_key_openssh
+}
+
+resource "aws_security_group" "trendgetter_ec2_sg" {
+  name        = "c18-trendgetter-ec2-sg"
+  description = "Allows connection to database from EC2 instance"
+  vpc_id      = data.aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "ec2" {
+  ami                         = "ami-0f4f4482537714bd9" 
+  instance_type               = "t3.nano"
+  subnet_id                   = data.aws_subnet.public_1.id  
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.trendgetter_ec2_key.key_name
+  vpc_security_group_ids      = [aws_security_group.trendgetter_ec2_sg.id]
+
+  tags = {
+    Name = "c18-trendgetter-extract-load-ec2"
+  }
+}
