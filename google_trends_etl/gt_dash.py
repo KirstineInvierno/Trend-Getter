c85592b48f4aa9016b9ad9f48c7ca6ddde027113
michaelgowie <michaelgowie1@gmail.com>
2025-07-31 12:36:00 +0100

    Adds google trends dashboard page

--- google_trends_etl/gt_dash.py
+++ google_trends_etl/gt_dash.py
@@ -1,30 +1,35 @@
-import datetime
+'''
+This is the tab for visualisations of the google trends data
+'''
 import streamlit as st
 import pandas as pd
 from pytrends.request import TrendReq
 
-if 'input' not in st.session_state:
-    st.session_state.input = 'trump'
-
 
 @st.cache_data
-def get_trends_data(topic=st.session_state.input) -> pd.DataFrame:
+def get_trends_data(topic: str) -> pd.DataFrame:
+    '''
+    Obtain dataframes to be cached
+    '''
     pytrends = TrendReq(hl='en-US', tz=360)
 
     pytrends.build_payload(kw_list=[topic], timeframe='today 12-m')
     df_weekly = pytrends.interest_over_time()
     pytrends.build_payload(kw_list=[topic], timeframe='now 7-d')
     df_daily = pytrends.interest_over_time()
-    df_daily['topic_name'] = topic
-    df_weekly['topic_name'] = topic
-    st.text(df_daily.columns)
-    df_daily['date'] = df_daily.index
-    df_weekly['date'] = df_weekly.index
-    df_daily.rename(columns={topic: 'hits'}, inplace=True)
-    df_weekly.rename(columns={topic: 'hits'}, inplace=True)
-    df_daily = df_daily[['date', 'hits', 'topic_name']]
-    df_weekly = df_weekly[['date', 'hits', 'topic_name']]
-    return df_weekly, df_daily
+
+    return prepare_df(df_weekly, topic), prepare_df(df_daily, topic)
+
+
+def prepare_df(df: pd.DataFrame, topic: str) -> pd.DataFrame:
+    '''
+    Prepares the dataframes to be used in the visualisations
+    '''
+    df['topic_name'] = topic
+    df['date'] = df.index
+    df.rename(columns={topic: 'hits'}, inplace=True)
+    df = df[['date', 'hits', 'topic_name']]
+    return df
 
 
 def filter_trend_options(df: pd.DataFrame, unique_key: str) -> pd.DataFrame:
@@ -42,71 +47,71 @@ def filter_trend_options(df: pd.DataFrame, unique_key: str) -> pd.DataFrame:
     return data
 
 
-def weekly_line_chart(df: pd.DataFrame):
+def weekly_line_chart(df: pd.DataFrame) -> None:
     '''
     Creates a line chart which shows hourly sales for each truck
     '''
 
-    data = filter_trend_options(df, unique_key='topic')
+    data = filter_trend_options(df, unique_key='unique1')
 
     st.line_chart(data, x='date', x_label='Week Start Date',  y='hits',
                   y_label='Hit Rate', color='topic_name')
 
 
-def daily_line_chart(df: pd.DataFrame):
+def daily_line_chart(df: pd.DataFrame) -> None:
     '''
     Creates a line chart which shows hourly sales for each topic
     '''
 
-    data = filter_trend_options(df, unique_key='topic2')
+    data = filter_trend_options(df, unique_key='unique2')
 
     st.line_chart(data, x='date', x_label='Hour Start Date',  y='hits',
-                  y_label='Hits Rate', color='topic_name')
+                  y_label='Hit Rate', color='topic_name')
 
 
-def topic_input():
+def topic_input() -> str:
     '''
     Takes input as topic
     '''
-    input_topic = st.text_input('Input a Topic to View')
-    return input_topic
+    topic = st.text_input('Input a Topic to View.')
+    return topic
 
 
-def increment_counter():
-    st.session_state.count += 1
+def initialise_session_state_dataframes() -> None:
+    '''
+    Sets up the dataframes to be added to when the session begins
+    '''
+    if 'weekly_df' not in st.session_state:
+        st.session_state.weekly_df = pd.DataFrame(
+            data={'date': [], 'topic_name': [], 'hits': []})
+    if 'daily_df' not in st.session_state:
+        st.session_state.daily_df = pd.DataFrame(
+            data={'date': [], 'topic_name': [], 'hits': []})
 
 
-@st.cache_data
-def concat_dfs(df1, df2):
-    return pd.concat(df1, df2)
+def update_dataframes(topic: str) -> None:
+    '''
+    Updates dataframes with new topic data
+    '''
+    new_dfs = get_trends_data(topic)
+
+    st.session_state.weekly_df = pd.concat([
+        st.session_state.weekly_df, new_dfs[0]])
+    st.session_state.daily_df = pd.concat([
+        st.session_state.daily_df, new_dfs[1]])
 
 
 if __name__ == '__main__':
-    if 'weekly_df' not in st.session_state:
-        st.session_state.weekly_df = pd.DataFrame(
-            data={'date': [1], 'topic_name': [1], 'hits': [1]})
-    if 'count' not in st.session_state:
-        st.session_state.count = 0
-    st.text(st.session_state.count)
-    if st.session_state.count == 0:
-        weekly_df = pd.DataFrame(
-            data={'date': [1], 'topic_name': [1], 'hits': [1]})
-        daily_df = pd.DataFrame(
-            data={'date': [2], 'topic_name': [2], 'hits': [2]})
-        increment_counter()
-    st.text(st.session_state.count)
-    topic = st.text_input(
-        'Input a Topic to View')
-    # st.dataframe(daily_df)
-    st.dataframe(st.session_state.weekly_df)
+    st.title('Google Search Trends')
 
-    if topic:
-        # dfs = get_trends_data()
-        st.session_state.weekly_df = pd.concat([
-            st.session_state.weekly_df, st.session_state.weekly_df])
+    initialise_session_state_dataframes()
+    st.text('''Please Input a topic to see the relevant search
+                trends over the last year and the last week.''')
+    input_topic = topic_input()
 
-        st.dataframe(st.session_state.weekly_df)
-        # st.dataframe(daily_df)
+    if input_topic:
+
+        update_dataframes(input_topic)
+
         weekly_line_chart(st.session_state.weekly_df)
-        # daily_line_chart(daily_df)
-        st.text(st.session_state.count)
+        daily_line_chart(st.session_state.daily_df)
