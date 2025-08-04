b279e531840e4dec4e90a8e7f94c08c6625aaf4b
michaelgowie <michaelgowie1@gmail.com>
2025-08-04 17:28:04 +0100

    Adds progress

--- bluesky_etl/threshold_check.py
+++ bluesky_etl/threshold_check.py
@@ -11,76 +11,88 @@ import json
 from load import DBLoader
 
 
-def get_subscriptions_data(loader: DBLoader) -> dict:
-    '''
-    Obtains the current notification subscriptions from RDS
-    '''
-    conn = loader.get_sql_conn()
-    subs_df = pd.read_sql('''SELECT user_id, topic_id, threshold FROM bluesky.user_topic''',
-                          con=conn)
+class DataGetter():
 
-    subs_dict = subs_df.to_dict('index')
-    return subs_dict
+    '''
+    Class which interacts with db
+    '''
+
+    def __init__(self, loader: DBLoader):
+        '''
+        Initializes DataGetter
+        '''
+        self.subscriptions_dict = self.get_subscriptions_data(loader)
+        self.mentions_df = self.get_recent_mentions(loader)
+
+    def get_subscriptions_data(self, loader: DBLoader) -> dict:
+        '''
+        Obtains the current notification subscriptions from RDS
+        '''
+        conn = loader.get_sql_conn()
+        subs_df = pd.read_sql('''SELECT user_id, email, topic_id, topic_name, threshold 
+                                FROM bluesky.user_topic 
+                                    JOIN bluesky.users USING(user_id)
+                                    JOIN bluesky.topic USING(topic_id)''',
+                              con=conn)
+
+        subs_dict = subs_df.to_dict('index')
+        return subs_dict
+
+    def get_now(self) -> datetime:
+        '''
+        Returns the datetime object for now
+        '''
+        return datetime.now()
+
+    def get_ten_minutes_ago(self) -> str:
+        '''
+        Returns a string of timestamp ten minutes ago for use in the sql query
+        '''
+        now = self.get_now()
+        ten_ago = now - timedelta(minutes=10)
+        ten_ago_string = ten_ago.strftime("%Y-%m-%d %H:%M:%S")
+        return ten_ago_string
+
+    def get_recent_mentions(self, loader: DBLoader) -> pd.DataFrame:
+        '''
+        Gets mentions from previous ten minute cycle from RDS
+        '''
+        time_str = self.get_ten_minutes_ago()
+        conn = loader.get_sql_conn()
+        mentions_df = pd.read_sql(f"""SELECT mention_id, topic_id FROM bluesky.mention WHERE timestamp > '{time_str}'""",
+                                  con=conn)
+        return mentions_df
 
 
-def get_now() -> datetime:
-    '''
-    Returns the datetime object for now
-    '''
-    return datetime.now()
+class ThresholdChecker():
 
+    def check_threshold(self, threshold_dict: dict, mentions_df: pd.DataFrame) -> bool:
+        '''
+        Returns true if threshold is met, false otherwise
+        '''
+        filtered_df = mentions_df[mentions_df['topic_id']
+                                  == threshold_dict['topic_id']]
+        mentions_count = len(filtered_df)
+        if mentions_count > threshold_dict['threshold']:
+            return True
+        return False
 
-def get_ten_minutes_ago(now: datetime) -> str:
-    '''
-    Returns a string of timestamp ten minutes ago for use in the sql query
-    '''
-    ten_ago = now - timedelta(minutes=10)
-    ten_ago_string = ten_ago.strftime("%Y-%m-%d %H:%M:%S")
-    return ten_ago_string
-
-
-def get_recent_mentions(loader: DBLoader, time_str: str) -> pd.DataFrame:
-    '''
-    Gets mentions from previous ten minute cycle from RDS
-    '''
-    conn = loader.get_sql_conn()
-    subs_df = pd.read_sql(f"""SELECT mention_id, topic_id FROM bluesky.mention WHERE timestamp > '{time_str}'""",
-                          con=conn)
-    return subs_df
-
-
-def check_threshold(threshold_dict: dict, mentions_df: pd.DataFrame) -> bool:
-    '''
-    Returns true if threshold is met, false otherwise
-    '''
-    filtered_df = mentions_df[mentions_df['topic_id']
-                              == threshold_dict['topic_id']]
-    mentions_count = len(filtered_df)
-    if mentions_count > threshold_dict['threshold']:
-        return True
-    return False
-
-
-def check_all_thresholds(subscriptions_dict: dict, mentions_df: pd.DataFrame):
-    '''
-    Returns a list of threshold dictionaries which have been met
-    '''
-    thresholds_met = []
-    for row in subscriptions_dict.keys():
-        if check_threshold(subscriptions_dict[row], mentions_df):
-            thresholds_met.append(subscriptions_dict[row])
-    return thresholds_met
+    def check_all_thresholds(self, subscriptions_dict: dict, mentions_df: pd.DataFrame):
+        '''
+        Returns a list of threshold dictionaries which have been met
+        '''
+        thresholds_met = []
+        for row in subscriptions_dict.keys():
+            if self.check_threshold(subscriptions_dict[row], mentions_df):
+                thresholds_met.append(subscriptions_dict[row])
+        return thresholds_met
 
 
 if __name__ == '__main__':
     loader = DBLoader()
-    sdict = get_subscriptions_data(loader)
-    now = get_now()
-    weeks_ago = now - timedelta(weeks=10)
-    weeks_ago = weeks_ago.strftime("%Y-%m-%d %H:%M:%S")
-    tma = get_ten_minutes_ago(now)
-    mdf = get_recent_mentions(loader, weeks_ago)
-    print(check_all_thresholds(sdict, mdf))
-    client = boto3.client('sns')
-    response = client.publish(PhoneNumber='+447512843683',
-                              Message=json.dumps({'event': 'text', 'msg': 'hello'}))
+    dgetter = DataGetter(loader)
+    print(dgetter.subscriptions_dict)
+    tchecker = ThresholdChecker()
+
+    print(tchecker.check_all_thresholds(
+        dgetter.subscriptions_dict, dgetter.mentions_df))
