97fdaa67681dc139ace158c0d170aba31d6bbc21
KirstineInvierno <trainee.kirstine.invierno@sigmalabs.co.uk>
2025-08-04 16:14:12 +0100

    Added logging

--- bluesky_etl/load.py
+++ bluesky_etl/load.py
@@ -1,40 +1,40 @@
-'''
-This script loads a prepared message dataframe into the RDS database 
-'''
+"""Script to load a prepared message dataframe into the RDS database."""
 
 from os import environ
 import pandas as pd
 import sqlalchemy
 import psycopg2
+import logging
 from dotenv import load_dotenv
-
-
+from sqlalchemy.engine import Engine
+from sqlalchemy.exc import SQLAlchemyError
 
 load_dotenv()
 
+logging.basicConfig(
+    format="%(levelname)s | %(asctime)s | %(message)s", level=logging.INFO)
 
 class DBLoader():
-    '''
-    Class which deals with the upload of the data
-    '''
+    """Handles upload of data to RDS."""
 
     def get_sql_conn(self):
-        '''
-        Returns connection to RDS
-        '''
-        host = environ["DB_HOST"]
-        user = environ["DB_USER"]
-        password = environ["DB_PASSWORD"]
-        database = environ["DB_NAME"]
-        engine = sqlalchemy.create_engine(
-            f"postgresql+psycopg2://{user}:{password}@{host}/{database}")
-        return engine
+        """Returns connection to RDS."""
+        try:
+            host = environ["DB_HOST"]
+            user = environ["DB_USER"]
+            password = environ["DB_PASSWORD"]
+            database = environ["DB_NAME"]
+            engine = sqlalchemy.create_engine(
+                f"postgresql+psycopg2://{user}:{password}@{host}/{database}")
+            logging.info("Successfully connected to the RDS.")
+            return engine
+        except SQLAlchemyError as e:
+            logging.error("Failed to connect to RDS. Error: %s", e)
+            raise
 
     def upload_df_to_mention(self, df: pd.DataFrame,
-                             engine: sqlalchemy.engine, schema: str) -> None:
-        '''
-        Performs the upload to the RDS
-        '''
+                             engine: Engine, schema: str) -> None:
+        """Uploads data to the RDS."""
         with engine.begin() as conn:
             df.to_sql(
                 'mention',
@@ -43,13 +43,11 @@ class DBLoader():
                 index=False,
                 schema=schema
             )
-            conn.commit()
+            logging.info("Uploaded %s rows to the RDS.", len(df))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     loader = DBLoader()
     df = pd.DataFrame()
-    print('connecting')
     con = loader.get_sql_conn()
-    print('connected')
-    loader.upload_df_to_mention(engine=con, df=df, schema='bluesky')
+    loader.upload_df_to_mention(engine=con, df=df, schema="bluesky")
