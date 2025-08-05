51f4e0a46b090c16f78dd12314b7d4c4b4a62aa8
michaelgowie <michaelgowie1@gmail.com>
2025-08-05 12:08:17 +0100

    Fixes comments

--- email_notifications/threshold_check.py
+++ email_notifications/threshold_check.py
@@ -8,23 +8,17 @@ import sqlalchemy
 
 class DataGetter():
 
-    '''
-    Class which interacts with db
-    '''
+    """Class which interacts with db"""
 
     def __init__(self):
-        '''
-        Initializes DataGetter
-        '''
+        """Initializes DataGetter"""
         load_dotenv()
         self.sql_conn = self.get_sql_conn()
         self.subscriptions_dict = self.get_subscriptions_data()
         self.mentions_df = self.get_recent_mentions()
 
     def get_subscriptions_data(self) -> dict:
-        '''
-        Obtains the current notification subscriptions from RDS
-        '''
+        """Obtains the current notification subscriptions from RDS"""
         subs_df = pd.read_sql('''SELECT user_id, email, topic_id,
                                 topic_name, threshold
                                 FROM bluesky.user_topic
@@ -36,9 +30,7 @@ class DataGetter():
         return subs_dict
 
     def get_sql_conn(self):
-        '''
-        Returns connection to RDS
-        '''
+        """Returns connection to RDS"""
         host = environ["DB_HOST"]
         user = environ["DB_USER"]
         password = environ["DB_PASSWORD"]
@@ -48,24 +40,18 @@ class DataGetter():
         return engine
 
     def get_now(self) -> datetime:
-        '''
-        Returns the datetime object for now
-        '''
+        """Returns the datetime object for now"""
         return datetime.now()
 
     def get_ten_minutes_ago(self) -> str:
-        '''
-        Returns a string of timestamp ten minutes ago for use in the sql query
-        '''
+        """Returns a string of timestamp ten minutes ago for use in the sql query"""
         now = self.get_now()
         ten_ago = now - timedelta(minutes=10)
         ten_ago_string = ten_ago.strftime("%Y-%m-%d %H:%M:%S")
         return ten_ago_string
 
     def get_recent_mentions(self) -> pd.DataFrame:
-        '''
-        Gets mentions from previous ten minute cycle from RDS
-        '''
+        """Gets mentions from previous ten minute cycle from RDS"""
         time_str = self.get_ten_minutes_ago()
         mentions_df = pd.read_sql(f"""SELECT mention_id, topic_id
                                   FROM bluesky.mention WHERE timestamp > '{time_str}'""",
@@ -74,14 +60,10 @@ class DataGetter():
 
 
 class ThresholdChecker():
-    '''
-    Class which checks whether the mentions threshold has been reached
-    '''
+    """Class which checks whether the mentions threshold has been reached"""
 
     def check_threshold(self, threshold_dict: dict, mentions_df: pd.DataFrame) -> int | bool:
-        '''
-        Returns true if threshold is met, false otherwise
-        '''
+        """Returns true if threshold is met, false otherwise"""
         filtered_df = mentions_df[mentions_df['topic_id']
                                   == threshold_dict['topic_id']]
         mentions_count = len(filtered_df)
@@ -90,9 +72,8 @@ class ThresholdChecker():
         return False
 
     def check_all_thresholds(self, subscriptions_dict: dict, mentions_df: pd.DataFrame):
-        '''
-        Returns a list of threshold dictionaries which have been met
-        '''
+        """Returns a list of threshold dictionaries which have been met"""
+
         thresholds_met = []
         for row in subscriptions_dict.keys():
             count = self.check_threshold(subscriptions_dict[row], mentions_df)
@@ -102,12 +83,3 @@ class ThresholdChecker():
                 print(row, self.check_threshold(
                     subscriptions_dict[row], mentions_df))
         return thresholds_met
-
-
-if __name__ == '__main__':
-    dgetter = DataGetter()
-    print(dgetter.subscriptions_dict)
-    tchecker = ThresholdChecker()
-
-    print(tchecker.check_all_thresholds(
-        dgetter.subscriptions_dict, dgetter.mentions_df))
--- email_notifications/threshold_ses.py
+++ email_notifications/threshold_ses.py
@@ -1,4 +1,4 @@
-'''Script sends email notifications to email list'''
+"""Script sends email notifications to email list"""
 from os import environ
 import boto3
 from dotenv import load_dotenv
@@ -10,20 +10,14 @@ load_dotenv()
 
 
 class Sender():
-    '''
-    Class which sends emails
-    '''
+    """Class which sends emails"""
 
     def __init__(self):
-        '''
-        Initializes instances with ses client
-        '''
+        """Initializes instances with ses client"""
         self.ses_client = self.get_ses_client()
 
     def create_email_from_dict(self, subscription_dict: dict) -> str:
-        '''
-        Creates HTML to be sent in email
-        '''
+        """Creates HTML to be sent in email"""
 
         subject = f"Activity Spike relating to {subscription_dict['topic_name']}"
 
@@ -43,9 +37,7 @@ class Sender():
         return email_dict
 
     def get_ses_client(self):
-        '''
-        Returns a ses client to use to send emails
-        '''
+        """Returns a ses client to use to send emails"""
         region = environ["AWS_DEFAULT_REGION"]
         access_key = environ["AWS_ACCESS_KEY_ID"]
         secret_key = environ['AWS_SECRET_ACCESS_KEY']
@@ -59,10 +51,7 @@ class Sender():
         return ses_client
 
     def send_email(self, subscription_dict: dict, email_dict: dict) -> None:
-        '''
-        Sends a notification email 
-        '''
-        print('sending')
+        """Sends a notification email"""
         try:
             response = self.ses_client.send_email(
                 Source=environ['SENDER_EMAIL'],
@@ -85,14 +74,11 @@ class Sender():
                     }
                 }
             )
-            print(response, 'sent')
         except ClientError:
             print(f'{subscription_dict['email']} not verified')
 
     def send_all_emails(self, subs_list: list[dict]):
-        '''
-        Calls send_email function for each notification required
-        '''
+        """Calls send_email function for each notification required"""
         for subscription in subs_list:
             email_dict = self.create_email_from_dict(subscription)
             self.send_email(subscription, email_dict)
@@ -100,13 +86,10 @@ class Sender():
 
 if __name__ == "__main__":
     dgetter = DataGetter()
-    print(dgetter.subscriptions_dict)
     tchecker = ThresholdChecker()
 
     subs = tchecker.check_all_thresholds(
         dgetter.subscriptions_dict, dgetter.mentions_df)
-
-    print(subs)
 
     sender = Sender()
     sender.send_all_emails(subs)
