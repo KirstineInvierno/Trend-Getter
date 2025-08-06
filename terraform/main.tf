64c602e518fca964aee260c1a48b494e0231f3c1
jacoblewis046 <jacoblewis046@gmail.com>
2025-08-06 10:53:35 +0100

    default region

--- terraform/main.tf
+++ terraform/main.tf
@@ -318,6 +318,7 @@ resource "aws_lambda_function" "lambda_function" {
       DB_SCHEMA             = var.DB_SCHEMA
       AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID
       AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
+      AWS_DEFAULT_REGION    = var.AWS_DEFAULT_REGION
     }
   }
 }
