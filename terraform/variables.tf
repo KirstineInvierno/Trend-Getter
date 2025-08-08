a5b6cb92b0a8bb15a278cbeb318ae9fe6cd8feb1
jacoblewis046 <jacoblewis046@gmail.com>
2025-08-08 10:56:47 +0100

    terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,18 +39,18 @@ variable "AWS_SECRET_ACCESS_KEY" {
 }
 
 variable "AWS_DEFAULT_REGION" {
-  type      = string
+  type        = string
   description = "The dafault region of the AWS services"
 }
 
 variable "BLUESKY_EMAIL" {
-  type      = string
+  type        = string
   description = "The email being used for the Bluesky bot"
-  sensitive = true
+  sensitive   = true
 }
 
 variable "BLUESKY_PASSWORD" {
-  type      = string
+  type        = string
   description = "The password being used for the Bluesky bot"
-  sensitive = true
+  sensitive   = true
 }
